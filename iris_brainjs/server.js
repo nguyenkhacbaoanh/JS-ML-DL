34880b00c9f673121b3fc9565e9c0e2609e5fbba
nguyen khac bao anh <khacbaoanh.nguyen@gmail.com>
2019-01-08 10:35:45 +0100

    first commit with Neural Networl -BrainJS

---
+++ iris_brainjs/server.js
@@ -0,0 +1,8 @@
+// Importer BrainJS
+const Brain = require("brain.js");
+
+// Créer un réseau de neuronnes
+const NeuralNetwork = new Brain.NeuralNetwork();
+
+// Afficher le réseau dans la console
+console.log(NeuralNetwork);
